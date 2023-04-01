cf936c00e2f20a165a5649929c04d081edc7b41b
Sara Rocha de Oliveira <sararocha878@gmail.com>
2023-04-01 14:16:32 -0300

    feat: implementation swagger

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,15 @@ import express, { Express } from "express";
 import cors from "cors";
 import routes from "./routes";
 
+import swaggerUi from "swagger-ui-express";
+import apiSchema from './api.schema.json';
 
 const app = express();
 
 app
   .use(cors())
   .use(express.json())
+  .use('/docs', swaggerUi.serve, swaggerUi.setup(apiSchema))
   .use(routes);
 
 export function init(): Promise<Express> {
